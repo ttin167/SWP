f2a2a24a5f557ed1a621773c23a2f166a115bb09
huynhngocduy <huynhngocduy3122021@gmail.com>
2025-09-28 12:18:39 +0700

    FileMoi

---
+++ demo2/src/main/java/com/ngocduy/fap/demo2/Demo2Application.java
@@ -0,0 +1,15 @@
+package com.ngocduy.fap.demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2Application.class, args)
+        ;
+        System.out.println("Hello World!");
+    }
+
+}
